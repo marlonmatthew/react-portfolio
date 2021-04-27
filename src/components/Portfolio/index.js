a99247ded7847870f718b950fa2a254be10f0ef1
Marlon Matthew <marlonbmatthew@gmail.com>
2021-04-27 11:25:18 -0700

    portfolio modified.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,242 +1,244 @@
 import React from "react";
 import "../../index.css";
 
-return (
-  <div>
-    <section class="container">
-      <div class="row">
-        <div class="col-md-auto mt-2 mb-4">
-          <h2 class="header">My Portfolio</h2>
-          <ul class="list-unstyled">
-            <li class="media">
-              <img
-                src="img/weather-dashboard.jpg"
-                class="mr-3"
-                alt="Weather Dashboard"
-                id="thumbnail"
-              ></img>
-              <div class="media-body">
-                <h5 class="mt-0 mb-1">Weather Dashboard</h5>
-                <p id="description">
-                  Search any city in the world to retrieve current weather
-                  conditions as well as a 5-day forecast. UCR Coding Bootcamp
-                  assignment #6.
-                </p>
-                <p>
-                  <a
-                    href="https://marlonmatthew.github.io/weather-dashboard/"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    Deployed Site
-                  </a>
-                </p>
-                <p>
-                  <a
-                    href="https://github.com/marlonmatthew/weather-dashboard"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    GitHub Repo
-                  </a>
-                </p>
-              </div>
-            </li>
-            <li class="media mb-4">
-              <img
-                src="img/ricemusic.jpg"
-                class="mr-3"
-                alt="ricemusic.net"
-                id="thumbnail"
-              ></img>
-              <div class="media-body">
-                <h5 class="mt-0 mb-1">ricemusic.net</h5>
-                <p id="description">
-                  San Diego bassist Rice Enright has been a staple in the music
-                  community for many years. It was important to create this site
-                  with WordPress and Elementor, which gave him the capability to
-                  make future updates and edits on his own.
-                </p>
-                <p>
-                  <a
-                    href="https://www.ricemusic.net"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    https://www.ricemusic.net
-                  </a>
-                </p>
-              </div>
-            </li>
-            <li class="media">
-              <img
-                src="img/burger-devourment-app.jpg"
-                class="mr-3"
-                alt="Burger Devourment App"
-                id="thumbnail"
-              ></img>
-              <div class="media-body">
-                <h5 class="mt-0 mb-1">Burger Devourment App</h5>
-                <p id="description">
-                  Add burgers to a database and change their devoured status.
-                </p>
-                <p>
-                  <a
-                    href="https://burger-devourment.herokuapp.com"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    https://burger-devourment.herokuapp.com
-                  </a>
-                </p>
-                <p>
-                  <a
-                    href="https://github.com/marlonmatthew/burger"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    GitHub Repo
-                  </a>
-                </p>
-              </div>
-            </li>
-            <li class="media">
-              <img
-                src="img/will-it-stream.jpg"
-                class="mr-3"
-                alt="Will It Stream"
-                id="thumbnail"
-              ></img>
-              <div class="media-body">
-                <h5 class="mt-0 mb-1">willitstream.com</h5>
-                <p id="description">
-                  Search for your favorite movie or show titles and see where
-                  they are streaming. UCR Coding Bootcamp project #1.
-                </p>
-                <p>
-                  <a
-                    href="https://willitstream.com"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    https://willitstream.com
-                  </a>
-                </p>
-                <p>
-                  <a
-                    href="https://github.com/marlonmatthew/will-it-stream"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    GitHub Repo
-                  </a>
-                </p>
-              </div>
-            </li>
-            <li class="media">
-              <img
-                src="img/daily-planner.jpg"
-                class="mr-3"
-                alt="Daily Planner UCA6"
-                id="thumbnail"
-              ></img>
-              <div class="media-body">
-                <h5 class="mt-0 mb-1">Daily Planner UCA6</h5>
-                <p id="description">
-                  A 24 hour daily planner with an Amazon theme. UCR Coding
-                  Bootcamp assignment #5.
-                </p>
-                <p>
-                  <a
-                    href="https://marlonmatthew.github.io/daily-planner-uca6/"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    Deployed Site
-                  </a>
-                </p>
-                <p>
-                  <a
-                    href="https://github.com/marlonmatthew/daily-planner-uca6"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    GitHub Repo
-                  </a>
-                </p>
-              </div>
-            </li>
-            <li class="media">
-              <img
-                src="img/javascript-quiz.jpg"
-                class="mr-3"
-                alt="JavaScript Quiz"
-                id="thumbnail"
-              ></img>
-              <div class="media-body">
-                <h5 class="mt-0 mb-1">JavaScript Quiz</h5>
-                <p id="description">
-                  A 5 question multiple choice quiz with high score
-                  functionality. UCR Coding Bootcamp assignment #4.
-                </p>
-                <p>
-                  <a
-                    href="https://marlonmatthew.github.io/javascript-quiz/"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    Deployed Site
-                  </a>
-                </p>
-                <p>
-                  <a
-                    href="https://github.com/marlonmatthew/javascript-quiz"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    GitHub Repo
-                  </a>
-                </p>
-              </div>
-            </li>
-            <li class="media">
-              <img
-                src="img/password-generator.jpg"
-                class="mr-3"
-                alt="Password Generator"
-                id="thumbnail"
-              ></img>
-              <div class="media-body">
-                <h5 class="mt-0 mb-1">Password Generator</h5>
-                <p id="description">
-                  Generate a password based off certain user criteria. UCR
-                  Coding Bootcamp assignment #3.
-                </p>
-                <p>
-                  <a
-                    href="https://marlonmatthew.github.io/password-generator/"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    Deployed Site
-                  </a>
-                </p>
-                <p>
-                  <a
-                    href="https://github.com/marlonmatthew/password-generator"
-                    id="portfolio-link"
-                    target="_blank"
-                  >
-                    GitHub Repo
-                  </a>
-                </p>
-              </div>
-            </li>
-          </ul>
+function Portfolio() {
+  return (
+    <div>
+      <section class="container">
+        <div class="row">
+          <div class="col-md-auto mt-2 mb-4">
+            <h2 class="header">My Portfolio</h2>
+            <ul class="list-unstyled">
+              <li class="media">
+                <img
+                  src="img/weather-dashboard.jpg"
+                  class="mr-3"
+                  alt="Weather Dashboard"
+                  id="thumbnail"
+                ></img>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">Weather Dashboard</h5>
+                  <p id="description">
+                    Search any city in the world to retrieve current weather
+                    conditions as well as a 5-day forecast. UCR Coding Bootcamp
+                    assignment #6.
+                  </p>
+                  <p>
+                    <a
+                      href="https://marlonmatthew.github.io/weather-dashboard/"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      Deployed Site
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      href="https://github.com/marlonmatthew/weather-dashboard"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      GitHub Repo
+                    </a>
+                  </p>
+                </div>
+              </li>
+              <li class="media mb-4">
+                <img
+                  src="img/ricemusic.jpg"
+                  class="mr-3"
+                  alt="ricemusic.net"
+                  id="thumbnail"
+                ></img>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">ricemusic.net</h5>
+                  <p id="description">
+                    San Diego bassist Rice Enright has been a staple in the
+                    music community for many years. It was important to create
+                    this site with WordPress and Elementor, which gave him the
+                    capability to make future updates and edits on his own.
+                  </p>
+                  <p>
+                    <a
+                      href="https://www.ricemusic.net"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      https://www.ricemusic.net
+                    </a>
+                  </p>
+                </div>
+              </li>
+              <li class="media">
+                <img
+                  src="img/burger-devourment-app.jpg"
+                  class="mr-3"
+                  alt="Burger Devourment App"
+                  id="thumbnail"
+                ></img>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">Burger Devourment App</h5>
+                  <p id="description">
+                    Add burgers to a database and change their devoured status.
+                  </p>
+                  <p>
+                    <a
+                      href="https://burger-devourment.herokuapp.com"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      https://burger-devourment.herokuapp.com
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      href="https://github.com/marlonmatthew/burger"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      GitHub Repo
+                    </a>
+                  </p>
+                </div>
+              </li>
+              <li class="media">
+                <img
+                  src="img/will-it-stream.jpg"
+                  class="mr-3"
+                  alt="Will It Stream"
+                  id="thumbnail"
+                ></img>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">willitstream.com</h5>
+                  <p id="description">
+                    Search for your favorite movie or show titles and see where
+                    they are streaming. UCR Coding Bootcamp project #1.
+                  </p>
+                  <p>
+                    <a
+                      href="https://willitstream.com"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      https://willitstream.com
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      href="https://github.com/marlonmatthew/will-it-stream"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      GitHub Repo
+                    </a>
+                  </p>
+                </div>
+              </li>
+              <li class="media">
+                <img
+                  src="img/daily-planner.jpg"
+                  class="mr-3"
+                  alt="Daily Planner UCA6"
+                  id="thumbnail"
+                ></img>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">Daily Planner UCA6</h5>
+                  <p id="description">
+                    A 24 hour daily planner with an Amazon theme. UCR Coding
+                    Bootcamp assignment #5.
+                  </p>
+                  <p>
+                    <a
+                      href="https://marlonmatthew.github.io/daily-planner-uca6/"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      Deployed Site
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      href="https://github.com/marlonmatthew/daily-planner-uca6"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      GitHub Repo
+                    </a>
+                  </p>
+                </div>
+              </li>
+              <li class="media">
+                <img
+                  src="img/javascript-quiz.jpg"
+                  class="mr-3"
+                  alt="JavaScript Quiz"
+                  id="thumbnail"
+                ></img>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">JavaScript Quiz</h5>
+                  <p id="description">
+                    A 5 question multiple choice quiz with high score
+                    functionality. UCR Coding Bootcamp assignment #4.
+                  </p>
+                  <p>
+                    <a
+                      href="https://marlonmatthew.github.io/javascript-quiz/"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      Deployed Site
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      href="https://github.com/marlonmatthew/javascript-quiz"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      GitHub Repo
+                    </a>
+                  </p>
+                </div>
+              </li>
+              <li class="media">
+                <img
+                  src="img/password-generator.jpg"
+                  class="mr-3"
+                  alt="Password Generator"
+                  id="thumbnail"
+                ></img>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">Password Generator</h5>
+                  <p id="description">
+                    Generate a password based off certain user criteria. UCR
+                    Coding Bootcamp assignment #3.
+                  </p>
+                  <p>
+                    <a
+                      href="https://marlonmatthew.github.io/password-generator/"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      Deployed Site
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      href="https://github.com/marlonmatthew/password-generator"
+                      id="portfolio-link"
+                      target="_blank"
+                    >
+                      GitHub Repo
+                    </a>
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
-  </div>
-);
+      </section>
+    </div>
+  );
+}
 
 export default Portfolio;
