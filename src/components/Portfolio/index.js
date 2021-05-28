03e696d6dc94130b1c68f33111ef05a9d8283a02
Marlon Matthew <mbm.drums@gmail.com>
2021-05-28 13:09:27 -0700

    Update index.js
    skills added

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -237,6 +237,14 @@ function Portfolio() {
               </li>
             </ul>
           </div>
+    <div className="col-md-auto mt-2 mb-4">
+            <h2 className="header">Technical Skills</h2>
+    <p id="description">
+    Languages: Javascript ES6+, CSS3, HTML5, SQL, NoSQL 
+
+Skills: GitHub, MySQL, MongoDB, ReactJS, Node.js, Express, Handlebars.js, JQuery, Bootstrap, SEO, Copywriting, Hootsuite, MailChimp, WordPress, Elementor, Adobe Photoshop, Microsoft Office 
+    </p>
+    </div>
         </div>
       </section>
     </div>
